c281f525f24321e0d3a0233ed8b5016957fa0416
Pratik Kumawat <kumawat10@gmail.com>
2023-03-30 17:58:40 +0530

    Thymeleaf template, Cache disable

--- src/main/java/com/pratik/springboot/rest/controller/HelloController.java
+++ src/main/java/com/pratik/springboot/rest/controller/HelloController.java
@@ -2,6 +2,7 @@ package com.pratik.springboot.rest.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloController {
@@ -9,6 +10,12 @@ public class HelloController {
     @RequestMapping("/hello")
     public String hello() {
         return "hello";
+    }
+    @RequestMapping("/sendData")
+    public ModelAndView sendData() {
+        ModelAndView mav = new ModelAndView("data");
+        mav.addObject("message", "If you are good at something, never do it for free");
+        return mav;
     }
 
 }
